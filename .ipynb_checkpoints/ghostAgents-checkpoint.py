89c927876828650e6a1a5f2cb3dfd0d91fa2aa33
westonruths <wruths@gmail.com>
2022-03-08 21:10:48 -0500

    updates to transition functions

--- .ipynb_checkpoints/ghostAgents-checkpoint.py
+++ .ipynb_checkpoints/ghostAgents-checkpoint.py
@@ -47,56 +47,29 @@ class RandomGhost(GhostAgent):
         dist.normalize()
         return dist
 
+class RandomGhostN(GhostAgent):
+    "A ghost that chooses a legal action uniformly at random."
 
-class DirectionalGhost8(GhostAgent):
+    def getDistribution(self, state):
+        dist = util.Counter()
+        for a in state.getLegalActions(self.index):
+            if a == "North":
+                dist[a] = 5
+            elif a == "South":
+                dist[a] = 1
+            elif a == "East":
+                dist[a] = 1
+            elif a == "West":
+                dist[a] = 1
+ 
+        dist.normalize()
+        return dist    
+
+
+class DirectionalGhost80(GhostAgent):
     "A ghost that prefers to rush Pacman, or flee when scared."
 
     def __init__(self, index, prob_attack=0.8, prob_scaredFlee=0.8):
-        self.index = index
-        self.prob_attack = prob_attack
-        self.prob_scaredFlee = prob_scaredFlee
-
-    def getDistribution(self, state):
-        # Read variables from state
-        ghostState = state.getGhostState(self.index)
-        legalActions = state.getLegalActions(self.index)
-        pos = state.getGhostPosition(self.index)
-        isScared = ghostState.scaredTimer > 0
-
-        speed = 1
-        if isScared:
-            speed = 0.5
-
-        actionVectors = [Actions.directionToVector(
-            a, speed) for a in legalActions]
-        newPositions = [(pos[0] + a[0], pos[1] + a[1]) for a in actionVectors]
-        pacmanPosition = state.getPacmanPosition()
-
-        # Select best actions given the state
-        distancesToPacman = [manhattanDistance(
-            pos, pacmanPosition) for pos in newPositions]
-        if isScared:
-            bestScore = max(distancesToPacman)
-            bestProb = self.prob_scaredFlee
-        else:
-            bestScore = min(distancesToPacman)
-            bestProb = self.prob_attack
-        bestActions = [action for action, distance in zip(
-            legalActions, distancesToPacman) if distance == bestScore]
-
-        # Construct distribution
-        dist = util.Counter()
-        for a in bestActions:
-            dist[a] = bestProb / len(bestActions)
-        for a in legalActions:
-            dist[a] += (1 - bestProb) / len(legalActions)
-        dist.normalize()
-        return dist
-
-class DirectionalGhost7(GhostAgent):
-    "A ghost that prefers to rush Pacman, or flee when scared."
-
-    def __init__(self, index, prob_attack=0.7, prob_scaredFlee=0.7):
         self.index = index
         self.prob_attack = prob_attack
         self.prob_scaredFlee = prob_scaredFlee
